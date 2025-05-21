7c9b6bc9e731d6c84f225037a754623a66f82bec
TruongHoangPhuc_2280602486 <hoangphuc15072004@gmail.com>
2025-05-21 10:28:34 +0700

    [add] lab-02 vigenere

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 # Giả sử api.py nằm ở your_project_root/
 from caesar.caesar_cipher import CaesarCipher
+from cipher.vigenere import VigenereCipher
 
 app = Flask(__name__)
 
 # CAESAR CIPHER ALGORITHM
 caesar_cipher = CaesarCipher()
-
+vigenere_cipher = VigenereCipher()
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
@@ -22,6 +23,23 @@ def caesar_decrypt():
     key = int(data['key'])
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key) # Bây giờ sẽ gọi đúng phương thức của đối tượng
     return jsonify({'decrypted_message': decrypted_text})
+
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt(): # Đổi tên hàm để tránh trùng lặp nếu có
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt(): # Đổi tên hàm để tránh trùng lặp nếu có
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
 
 # main function
 if __name__ == "__main__":
